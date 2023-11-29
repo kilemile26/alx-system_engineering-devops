805ce63e0b2e2708f08494a6f88af19d60423488
kilemile26 <harunakilemile@example.com>
2023-11-28 17:13:24 -0800

    repetition1

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+# Check if an argument is provided
+if ARGV.empty?
+  puts "Please provide an argument."
+else
+  # Define the regular expression using Oniguruma syntax with positive lookahead
+  regex = /t{2,5}/
+
+  # Get the argument
+  input = ARGV[0]
+
+  # Check if there are matches and print them
+  if (match = input.match(regex))
+    puts #{match[0]}
+  else
+    puts ""
+  end
+end
