1ca6fd903d140d2c450a65196b352dea102f8edf
kilemile26 <harunakilemile@example.com>
2023-12-01 16:27:36 -0800

    more on regex

---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+# Define the regular expression using Oniguruma syntax with positive lookahead
+  regex = /h^\d{10,10}$/
+
+# Check if an argument is provided
+if ARGV.empty?
+  puts "Please provide an argument."
+else
+  # Get the argument
+  input = ARGV[0]
+
+  # Find all matches in the input string
+  matches = input.scan(regex)
+
+  # Check if there are matches and print them
+  if matches
+    puts matches.join
+  else
+    puts ""
+  end
+end
---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+# Define the regular expression using Oniguruma syntax with positive lookahead
+  regex = /[A-Z]*/
+
+# Check if an argument is provided
+if ARGV.empty?
+  puts "Please provide an argument."
+else
+  # Get the argument
+  input = ARGV[0]
+
+  # Find all matches in the input string
+  matches = input.scan(regex)
+
+  # Check if there are matches and print them
+  if matches
+    puts matches.join
+  else
+    puts ""
+  end
+end
