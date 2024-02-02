87d4240fb6da696fc9782725de9a573edde8cd6f
kilemile26 <harunakilemile@example.com>
2024-02-02 06:10:48 -0800

    create3.

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,7 @@
 # creating a file in a given path with permissions and other features
-file{'/tmp/school':
-ensure     => 'file'
-content    => 'I love Puppet'
-mode       => '0744'
-owner      => 'www-data'
-group      => 'www-data'
+file { '/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
-
