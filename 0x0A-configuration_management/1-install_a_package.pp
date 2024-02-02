f52895e5809de5c66d4ee6faa5a5e8fd91693e30
kilemile26 <harunakilemile@example.com>
2024-02-02 06:57:14 -0800

    ins

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
+#!/usr/bin/pup
 # installing flask from pip3 on the server using puppet manifesto code
-package { 'pipflask':
+package { 'flask':
   ensure   => '2.1.0',
   name     => 'flask',
-  provider => 'pip3',
+  provider => 'pip3'
 }
