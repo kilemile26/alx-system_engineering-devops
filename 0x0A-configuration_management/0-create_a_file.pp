7daa0224ddb6507bc333c2d478f55df6787ba256
kilemile26 <harunakilemile@example.com>
2024-02-02 05:55:13 -0800

    create1

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creating a file in a given path with permissions and other features
 file{'/tmp/school':
-content => "I love Puppet"
-permission => "0744"
-owner => "www-data"
-group => "www-data"
+content        => 'I love Puppet'
+permission     => '0744'
+owner          => 'www-data'
+group          => 'www-data'
 }
 
